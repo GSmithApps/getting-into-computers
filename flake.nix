0ab14a078743e5e6356122420239093340630dc1
GSmithApps <14.gsmith.14@gmail.com>
2024-10-27 22:13:29 -0500

    moved

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "reveal.js environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+          ];
+          shellHook = ''
+            echo "Welcome to the reveal development environment!"
+            echo "Node.js version: $(node --version)"
+          '';
+        };
+      }
+    );
+}
